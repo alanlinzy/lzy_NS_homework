a44f0c77446bca82f7f838de64eebb491e0aff27
alanlinzy <34940728+alanlinzy@users.noreply.github.com>
2019-09-11 11:20:32 +0800

    03try

--- 03ER/ERclient.py
+++ 03ER/ERclient.py
@@ -1,45 +1,58 @@
 import socket
 import time  
 import asyncio
+import re
 
 class clientProtocol(asyncio.Protocol):
-    def __init__(self,message,loop):
-        self.message = message
-
-        self.loop = loop
-        self.transport = None
+    def __init__(self):
+        #self.message = message
+        self.recv = ""
+        #self.loop = loop
+        #self.transport = None
     
     def connection_made(self,transport):
         self.transport = transport
-
-        #self.transport.write(self.message.encode())
-        #print(self.message)
+        message = "SUBMIT,{ziyang lin},{zlin32@jh,edu},{2},{54216}"
+        self.transport.write(message.encode())
+        respon = re.search(r"OK",self.recv)
+        print(respon)
+        if respon:
+            message = "look mirror<EOL>\nget hairpin<EOL>\nunlock door with hairpin<EOL>\nopen door<EOL>\n"
+            self.transport.write(message.encode())
+            #print(self.message)
+        else:
+            print('fail')
             
     def data_received(self,data):
-        print(data.decode())
-        self.transport.close()
+        self.recv = data.decode()
+        print(self.recv)
+        #self.transport.close()
 
-    def connection_lost(self,exc):
 
-        self.loop.stop()
     
-
+'''
 
 async def main(loop):
-    message = ""
+    message = "SUBMIT,{ziyang lin},{zlin32@jh,edu},{2},{54216}<EOL>\n"
     transport,protocol = await loop.create_connection(
         lambda: clientProtocol(message,loop),'192.168.200.52',19003)
-    info = "SUBMIT,{ziyang lin},{zlin32@jh,edu},{2},{54216}<EOL>\n"
+    
     message = "look mirror<EOL>\nget hairpin<EOL>\nunlock door with hairpin<EOL>\nopen door<EOL>\n"
-    transport.write(info.encode())
-    time.sleep(0.5)
     #print(transport.data)
     transport.write(message.encode())
-        
+'''      
 if __name__=="__main__":
     loop = asyncio.get_event_loop()    
-    
-    loop.run_until_complete(main(loop))
-    #loop.run_forever()
+    coro = loop.create_connection(clientProtocol,'127.0.0.1',54216)
+    client = loop.run_until_complete(coro)
+
+    try:
+        loop.run_forever()
+	
+    except KeyboardInterrupt:
+        pass
+
+    client.close()
+    loop.run_until_complete(client.close())
     loop.close()
-   
+ 
--- 03ER/ERserver.py
+++ 03ER/ERserver.py
@@ -9,22 +9,19 @@ class server(asyncio.Protocol):
         peername = transport.get_extra_info("peername")
         print(peername)
         self.transport = transport
-
-    
-            
+        self.game = ERM.EscapeRoomGame(output = self.write_func)
+        self.game.create_game(cheat=True)
+        self.game.start()
+        
     def write_func(self,message):
         #socket.send()
-
         self.transport.write(message.encode())
         print(message)
     
     def data_received(self,message):
         print(message.decode())
-        game = ERM.EscapeRoomGame(output = self.write_func )
-        game.create_game(cheat=True)
-        game.start()
         #print(s.recv(1024))
-        while game.status == "playing":
+        while self.game.status == "playing":
             #command = input(">> ")
             #self.conn.send(b'>>')
             data = message# this could be multiple messages
@@ -36,9 +33,9 @@ class server(asyncio.Protocol):
                 if line !="":
                     # process each line
                     command = line
-                    output = game.command(command)
+                    output = self.game.command(command)
                 
-                
+                   
         
         
 if __name__=="__main__":
@@ -48,7 +45,7 @@ if __name__=="__main__":
     server = loop.run_until_complete(c)
 
     # Serve requests until Ctrl+C is pressed
-    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    #print('Serving on {}'.format(server.sockets[0].getsockname()))
     try:
         loop.run_forever()
     except KeyboardInterrupt:
